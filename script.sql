d62c3b7ffb1cceb2bc32c9ca163990db603ee5b9
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2023-06-12 21:20:13 -0300

    Add files via upload

--- script.sql
+++ script.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.33, for macos13 (arm64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: localhost    Database: crud_manager
+-- Host: 127.0.0.1    Database: crud_manager
 -- ------------------------------------------------------
--- Server version	8.0.33
+-- Server version	5.5.5-10.4.28-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,18 +21,18 @@
 
 DROP TABLE IF EXISTS `companies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `companies` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `role` varchar(128) NOT NULL,
   `start` date NOT NULL,
   `end` date DEFAULT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `companies_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `companies` (
 
 LOCK TABLES `companies` WRITE;
 /*!40000 ALTER TABLE `companies` DISABLE KEYS */;
-INSERT INTO `companies` VALUES (1,'Deyvison ','CEO','2023-05-31',NULL,4),(2,'Júlia','COF','2023-06-05',NULL,3);
+INSERT INTO `companies` VALUES (1,'Deyvison ','CEO','2023-05-31',NULL,4),(2,'Júlia','COF','2023-06-05',NULL,3),(3,'Teste','Teste','2023-06-01','2023-06-09',3);
 /*!40000 ALTER TABLE `companies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,16 +51,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
   `post_date` date NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'Olá do Emerson','2023-05-29',1),(2,'Olá da Luiza','2023-05-29',2),(3,'Olá da Denise','2023-05-29',3),(4,'Olá do Noé','2023-05-29',4),(5,'Olá da Rosânia','2023-05-29',5),(6,'Olá da Rosânia 1','2023-05-29',5),(7,'Olá da Rosânia 2','2023-05-29',5),(8,'Olá da Rosânia 3','2023-05-29',5);
+INSERT INTO `posts` VALUES (1,'Olá do Emerson','2023-05-26',1),(2,'Olá da Luiza','2023-05-26',2),(3,'Olá da Denise','2023-05-26',3),(4,'Olá do Noé','2023-05-26',4),(5,'Olá da Rosânia','2023-05-26',5),(6,'Olá da Rosânia 1','2023-05-26',5),(7,'Olá da Rosânia 2','2023-05-26',5),(8,'Olá da Rosânia 3','2023-05-26',5);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,19 +79,19 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `suppliers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `suppliers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `cnpj` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `branch` varchar(100) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `cnpj` varchar(15) NOT NULL,
+  `branch` varchar(100) NOT NULL,
   `contract_start` date NOT NULL,
   `contract_end` date DEFAULT NULL,
-  `company_id` int NOT NULL,
+  `company_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `company_id` (`company_id`),
   CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +100,7 @@ CREATE TABLE `suppliers` (
 
 LOCK TABLES `suppliers` WRITE;
 /*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
-INSERT INTO `suppliers` VALUES (2,'Teste','00.000.000/0001-00','Teste','2023-06-07','2023-06-30',1),(3,'João','00.000.000/0001-00','Alimentício ','2023-06-05','2023-06-28',2);
+INSERT INTO `suppliers` VALUES (2,'Ana','00.000.000/0001','Teste','2023-06-07','2023-06-30',1),(3,'Teste','00.000.000/0001','Alimentício ','2023-06-05','2023-06-28',2);
 /*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,14 +110,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) NOT NULL,
   `sexo` enum('M','F') DEFAULT NULL,
   `email` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,4 +147,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-07 23:24:16
+-- Dump completed on 2023-06-12 21:19:01
